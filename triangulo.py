525b0b958bc9d26c88bd0ad467e1d160b7c80ae4
Lucas <lucas.vale.marx@gmail.com>
2026-09-13 21:49:06 -0300

    exercicio triangulo/trapezio

--- triangulo.py
+++ triangulo.py
@@ -1,3 +1,7 @@
 entrada = list(map(float,input().split()))
 a, b, c = entrada
 
+if a < (b + c) and b < (a + c) and c < (a + b):
+    print(f'perímetro: {a + b + c:.1f}')
+else:
+    print(f'área: {(a + b) * c / 2}')
