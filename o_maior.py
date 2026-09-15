78fc7023233405c9c338812a23294cdd86775180
Lucas <lucas.vale.marx@gmail.com>
2026-09-14 21:12:05 -0300

    arrumei o codigo

--- o_maior.py
+++ o_maior.py
@@ -1,8 +1,7 @@
 entrada = list(map(int,input().split()))
 a, b, c = entrada
-#fazer depois
 
 calculo = (a + b + abs(a - b)) // 2
 calculo_2 = (calculo + c + abs(calculo - c)) // 2
 
-print(calculo_2)
+print(f'{calculo_2} eh o maior')
